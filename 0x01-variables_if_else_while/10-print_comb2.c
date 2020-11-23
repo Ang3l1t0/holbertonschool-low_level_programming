51e8554c1e10bc8009b7eba5f3639956363f354d
Ang3l1t0 <1546@holbertonschool.com>
2020-11-22 22:12:16 -0500

    last number without spaces

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -9,10 +9,14 @@ int main(void)
 	int i;
 
 	for (i = 0; i < 100 ; i++)
-	{
+{
 		putchar((i / 10) + '0');
 		putchar((i % 10) + '0');
-		printf(" ");
+		if (i != 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
