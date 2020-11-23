09cc853522c3e6a2f9db2aa8f8d1e18ab8b4b49b
Ang3l1t0 <1546@holbertonschool.com>
2020-11-23 16:38:34 -0500

    putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * main - Entry level
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c[] = "Holberton";
+	int i = 0;
+
+	for (c[i] = c[i]; c[i]; i++)
+		_putchar(c[i]);
+	_putchar('\n');
+	return (0);
+}
