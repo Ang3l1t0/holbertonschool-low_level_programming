4c2390105724e0677ccf978991f571af75607662
Ang3l1t0 <1546@holbertonschool.com>
2020-11-22 20:33:12 -0500

    lower case alphabeth

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		printf("%c", c);
+	printf("\n");
+	return (0);
+}
