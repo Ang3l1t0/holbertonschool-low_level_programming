2bc81b27c8146c429208e036686baa3936ec04c3
Ang3l1t0 <1546@holbertonschool.com>
2020-11-25 10:55:27 -0500

    sinlgy list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_list - print all elements of a list
+ * @h: node
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	list_t * current = h;
+	size_t n;
+
+	for (n = 0; current != NULL; n++)
+	{
+		if (current->str == NULL)
+			printf("[0] (nil)\n");
+		if (current->str != NULL)
+			printf("[%d] %s\n", current->len, current->str);
+		current = current->next;
+	}
+	return (n);
+}
