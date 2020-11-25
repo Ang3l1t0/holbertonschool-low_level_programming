491df5ca0a07a2c9a9cde49d780030706e824633
Ang3l1t0 <1546@holbertonschool.com>
2020-11-25 11:39:55 -0500

    add node head

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * add_node - add node to the first position
+ * @head: first node
+ * @str: string name
+ * Return: number of nodes
+ **/
+list_t *add_node(list_t **head, const char *str)
+{
+	int n = 0;
+	list_t *new_node;
+
+	if (head == NULL || str == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	for ( ; str[n] != '\0'; )
+		n++;
+	new_node->len = n;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
