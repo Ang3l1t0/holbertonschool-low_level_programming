1f1aaa95eccf1d2fa2b47e7ce017ca32adb72a29
Ang3l1t0 <1546@holbertonschool.com>
2020-11-25 11:42:53 -0500

    free

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,7 +16,10 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
+	{
+		free(new_node);
 		return (NULL);
+	}
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
